3087e06ebb81b29564c4a63c3180302916523bfa
Titan3rd <sachuvnair08@gmail.com>
2026-07-09 11:16:15 +0530

    update

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,15 +2,17 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { createLmntApp } from "@titan3rd/framework-ui"
 import "@titan3rd/framework-ui/styles.css"
+import demoPackage from "../package.json"
 import { demoRoutes } from "./routes"
 
 const App = createLmntApp({
-  productRoutes: demoRoutes,
-  defaultRoute: "/demo/sample",
-})
+    productRoutes: demoRoutes,
+    defaultRoute: "/demo/sample",
+    productFrontendVersion: demoPackage.version,
+} as Parameters<typeof createLmntApp>[0] & { productFrontendVersion: string })
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
