ff322c0894c2eb55d87fa5a1843e0b4420b50ab7
Titan3rd <sachuvnair08@gmail.com>
2026-07-30 20:49:45 +0530

    update

--- ui/src/routes.tsx
+++ ui/src/routes.tsx
@@ -3,10 +3,14 @@ import { Navigate } from "react-router-dom"
 import { DemoHomePage } from "./pages/DemoHomePage"
 import { SamplePage } from "./pages/SamplePage"
 
-export const demoRoutes: LmntProductRoute[] = [
+type DemoProductRoute = LmntProductRoute & {
+  overrides?: string[]
+}
+
+export const demoRoutes = [
 
   { path: "/home", element: <Navigate to="/demo/home" replace /> },
-  { path: "/demo/home", element: <DemoHomePage /> },
+  { path: "/demo/home", element: <DemoHomePage />, overrides: ["/home"] },
   { path: "/demo/sample", element: <SamplePage /> },
 
-]
+] satisfies DemoProductRoute[]
